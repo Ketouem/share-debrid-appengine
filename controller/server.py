533fdf5c526f38914e398389344ef541fc41d538
Ketouem <ketouem@gmail.com>
2014-06-05 14:15:42 +0200

    Centralizing the env conf inside a file // Webpage stub // Handling get on main web page

--- controller/server.py
+++ controller/server.py
@@ -2,10 +2,17 @@ from google.appengine.api import users
 
 import webapp2
 
+from environment import JINJA_ENVIRONMENT
+
 
 class DebridPage(webapp2.RedirectHandler):
 
     def get(self):
         user = users.get_current_user()
 
-        # TODO: Add whitelisting logic
+        if user:
+            # TODO: Add whitelisting logic
+            template = JINJA_ENVIRONMENT.get_template('index.html')
+            self.response.write(template.render())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
